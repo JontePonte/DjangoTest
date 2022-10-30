bad797c8428a0c8bea5c06bceef4919faa0e8c44
JontePonte <john_nordstrand@hotmail.com>
2022-10-30 21:22:05 +0100

    Added database reference to todolist item in views, changed string format to f-string

--- DjangoTest/main/views.py
+++ DjangoTest/main/views.py
@@ -5,6 +5,8 @@ from .models import ToDoList, Item
 
 
 
-def index(response, id):
-    ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" % ls.name)
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse(f"<h1>{ls.name}</h1><br></br><p>{item.text}<p>")
+
