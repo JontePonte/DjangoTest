56536e1ba91526e38321f774ba7690a789bd227c
JontePonte <john_nordstrand@hotmail.com>
2022-11-01 15:09:42 +0100

    Removed unnecessary code from views

--- DjangoTest/main/views.py
+++ DjangoTest/main/views.py
@@ -10,7 +10,6 @@ def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
     if response.method == "POST":
-        print(response.POST)
         # Save checkbox status
         if response.POST.get("save"):
             for item in ls.item_set.all():
@@ -20,15 +19,12 @@ def index(response, id):
                     item.complete = False
                 item.save()
 
+        # Save new items
         elif response.POST.get("newItem"):
             txt = response.POST.get("new")
 
             if len(txt) > 2:
                 ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-
-            
 
     return render(response, "main/list.html", {"ls":ls})
 
