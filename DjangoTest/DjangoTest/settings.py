bb2cd74c316746321b0037e639b3f635388216b1
JontePonte <john_nordstrand@hotmail.com>
2022-11-02 09:24:55 +0100

    Added registration app to handle sign up, login, logout

--- DjangoTest/DjangoTest/settings.py
+++ DjangoTest/DjangoTest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig', # This adds dependencies from app to project
+    'register.apps.RegisterConfig', # This adds dependencies from app to project
 ]
 
 MIDDLEWARE = [
